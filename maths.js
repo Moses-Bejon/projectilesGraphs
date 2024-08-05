d0b88c98de931d29d140b2daad2b3519c32430db
Moses-Bejon <138283384+Moses-Bejon@users.noreply.github.com>
2024-08-05 16:20:10 +0100

    Added some matrix maths
    This was to accommodate the rotations that happen in task 11

--- maths.js
+++ maths.js
@@ -81,6 +81,86 @@ export function sigmoid(z){
     return 1/(1+Math.exp(-z*0.1))
 }
 
+export function multiplyMatrices(matrix1,matrix2){
+    const product = []
+
+    for (let i = 0; i<matrix1.length;i++){
+        product.push([])
+        for (let j = 0; j<matrix2[0].length;j++){
+            let total = 0
+            for (let k = 0; k <matrix2.length;k++){
+                total += matrix1[i][k]*matrix2[k][j]
+            }
+            product[i].push(total)
+        }
+    }
+
+    return product
+}
+
+export function multiplyMatrixWithVector(matrix,vector){
+    const product = []
+    const length = vector.length
+
+    for (let i = 0; i<length;i++){
+        let sum = 0
+        for (let j = 0; j<length;j++){
+            sum += matrix[i][j]*vector[j]
+        }
+        product.push(sum)
+    }
+
+    return product
+}
+
+// This function is 3 times faster than the above function.
+// As this is a speed-critical scenario it's worth cluttering the codebase for.
+export function multiply3x3MatrixWithVector(matrix,vector){
+    return [
+        matrix[0][0]*vector[0]+matrix[0][1]*vector[1]+matrix[0][2]*vector[2],
+        matrix[1][0]*vector[0]+matrix[1][1]*vector[1]+matrix[1][2]*vector[2],
+        matrix[2][0]*vector[0]+matrix[2][1]*vector[1]+matrix[2][2]*vector[2]
+    ]
+}
+
+export function findInverseOf3x3Matrix(matrix){
+    const a = matrix[0][0]
+    const b = matrix[0][1]
+    const c = matrix[0][2]
+    const d = matrix[1][0]
+    const e = matrix[1][1]
+    const f = matrix[1][2]
+    const g = matrix[2][0]
+    const h = matrix[2][1]
+    const i = matrix[2][2]
+
+    const A = e*i-f*h
+    const B = f*g-d*i
+    const C = d*h-e*g
+    const D = c*h-b*i
+    const E = a*i-c*g
+    const F = b*g-a*h
+    const G = b*f-c*e
+    const H = c*d-a*f
+    const I = a*e-b*d
+
+    const determinant = a*A + b*B + c*C
+
+    return [
+        [A/determinant,D/determinant,G/determinant],
+        [B/determinant,E/determinant,H/determinant],
+        [C/determinant,F/determinant,I/determinant]
+    ]
+}
+
+export function findInverseOfUnitary3x3Matrix(matrix){
+    return [
+        [matrix[0][0],matrix[1][0],matrix[2][0]],
+        [matrix[0][1],matrix[1][1],matrix[2][1]],
+        [matrix[0][2],matrix[1][2],matrix[2][2]]
+    ]
+}
+
 export function formatExponentWithSuperText(exponent){
     return "<sup>" + String(exponent) + "</sup>"
 }
